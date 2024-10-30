435743d6ba9ad0e1b7a9f7153dc3dda5b8f3b9f1
Carlos Alessandro <slim191shady@gmail.com>
2024-10-30 16:29:18 -0300

    Add files via upload

---
+++ src/br/alura/challenge/conversor/moedas/Main.java
@@ -0,0 +1,14 @@
+package br.alura.challenge.conversor.moedas;
+
+import br.alura.challenge.conversor.moedas.model.Console;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Console console = new Console();
+        console.callConsole();
+
+    }
+}
